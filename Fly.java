8da9c132fd901181f73f7b4c21c396118baa9bc6
Axcce1erat <40032656+Axcce1erat@users.noreply.github.com>
2019-11-25 14:44:29 +0100

    Update Fly.java

--- Fly.java
+++ Fly.java
@@ -5,16 +5,16 @@ public interface Fly{
     void takeOff();
 
     //ascends which takes an integer meters as an argument, and returns an integer
-    int ascends(int meters);
+    int ascend(int meters);
 
     //descends which takes an integer meters as an argument, and returns an integer
-    int descends(int meters);
+    int descend(int meters);
 
     //land with no arguments, which returns nothing
     void land();
 
     //glide, with no arguments, which returns nothing and has a default behaviour: displaying the text "glides into the air."
     default void glide(){
-        return "glides into the air";
+        System.out.println("glides in the air");
     }
 }
